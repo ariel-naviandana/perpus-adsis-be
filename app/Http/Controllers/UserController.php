d1014444cbeed20e9d47e6a7ccaed670b79b785a
Ariel2711 <arielnaviandanaputra@gmail.com>
2025-06-02 12:22:43 +0700

    update user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,7 +18,7 @@ class UserController extends Controller
         $data = $request->validate([
             'name' => 'required',
             'email' => 'required|email|unique:users',
-            'role' => 'required|in:admin,petugas,user',
+            'role' => 'required|in:admin,petugas,siswa',
             'password' => 'required|min:6'
         ]);
 
